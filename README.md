4543c60da01c7ae7f0e8a50a42102423c6171d0a
axels01 <axelstab01@gmail.com>
2023-05-02 15:46:59 +0200

    Added test.data, updated analysisTools.py and README.md

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # BachelorThesisTools
-A set of custom-made tools for my bachelor thesis project
+A set of custom-made tools for mine and [Slothmeister95](https://github.com/Slothmeister95)  bachelor thesis project.
+
+## File Tree Generator (generateFileTree.py)
+As part of the experiment a file tree was required, this script will generate a file tree at a desired location, several variables of the tree are editable via the last row of the script.
+
+```Python
+    makeFileTree(root_path, 2, 4, (6,25), 8, (14,25))
+```
+The variables and their corresponding action are explained in acomment above the *makeFileTree* function.
+## Personal Code Genrator (gnereatePersonalCode.py)
+A simple script to generate anonymous codes when conducting the experiment as to make the collected data anonymous. The codes follow the format XYZ-123, however this can be changed via the last line in the script.
+```Python
+    print(generateCode(3, 3, '-'))
+```
+It will however only output ascii uppercase characters followed by digits unless further modified.
+## Analysis Tools (analysisTools.py)
+### Depends on the following:
+* [Pandas](https://pandas.pydata.org/)
+* [Seaborn](https://seaborn.pydata.org/)
+* [Scipy](https://scipy.org/)
+* [Matplotlib](https://matplotlib.org/)
+* [Statsmodels](https://www.statsmodels.org/stable/index.html)
+
+### Implements the following functions:
+* Boxplot - for the entire data set, one box per index.
+* Q-Q plot - A simple Q-Q plot, will generate one per index.
+* Histogram - Will generate one per index.
+* Shapiro-Wilks test - Only outputs W- and p-values to console, one line per index.
+* Anova - *Not complete.*
+* Residual Q-Q - *Not complete.*
+* Pairwise Tukey HSD - *Not complete.*
+
+### Input Data and Usage
+Usage: *python3 analysisTools.py \<file> \<argument> \<plot title>*
+
+Arguments: *box, qq, shapiro, anova, t_hsd, resid*
+
+The input data is currently configed to follow a specific format, that of three columns, V, S and T spaced by one tab. See the test.data file for an example file. Not that the tab is imporatant and issues arose when trying to edit the file in Visual Studio Code as it seems to insert spaces and not tab.
--- analysisTools.py
+++ analysisTools.py
@@ -27,17 +27,27 @@ def melt(dFrame):
     return dFrame
 
 #Plots data, first row draws the boxplot, second row inserts the dots (data points)
-def boxPlot(dFrame):
+def boxPlot(dFrame, title = ""):
     sns.boxplot(x='Navigeringsmetod', y='Värde', data=dFrame)
     sns.swarmplot(x="Navigeringsmetod", y="Värde", data=dFrame)
+    plt.title("Boxplot " + title)
     plt.show()
     
 #Plots qq-plot
-def qq(dFrame):
+def qq(dFrame, title = ""):
     for index in dFrame:
         sm.qqplot(dFrame[index], line='s')
         #adds title
-        plt.title("qq-plot: " + index)
+        plt.title("Q-Q " + title + " " + index)
+        plt.show()
+
+def histogram(dFrame, title = ""):
+    for index in dFrame:
+        sns.histplot(
+            dFrame[index], kde=True,
+            stat="density", kde_kws=dict(cut=3),
+            alpha=.4, edgecolor=(1, 1, 1, .4))
+        plt.title("Histogram " + title + " " + index)
         plt.show()
 
 #Shapiro-Wilk test
@@ -59,8 +69,9 @@ def anova(dFrame, rt = False):
         print(anova_table)
 
 #residual qq plot
-def resid(dFrame):
+def resid(dFrame, title = ""):
     sm.qqplot(anova(dFrame, True), line='s')
+    plt.title("Residual Q-Q " + title)
     plt.show()
 
 
@@ -74,15 +85,15 @@ def help(message = ""):
     print("*"*50)
     if message != "":
         print(message)
-    print("Usage: python3 analysisTools.py <file> <argument>")
+    print("Usage: python3 analysisTools.py <file> <argument> <plot title>")
     print("Arguments: box, qq, shapiro, anova, t_hsd, resid")
     print("*"*50)
     exit()
 
-
 #Driver code
 if __name__ == '__main__':
     #read file into pandas dataframe
+    dFrame = None
     if len(sys.argv) > 1:
         #if first arg is help
         if sys.argv[1] in ['h', 'help', '-h', '--help']:
@@ -99,9 +110,20 @@ if __name__ == '__main__':
     #check if argument is given and running cooresponding function
     if len(sys.argv) > 2:
         if sys.argv[2] == 'box':
-            boxPlot(melt(dFrame))
+            if len(sys.argv) > 3:
+                boxPlot(melt(dFrame), sys.argv[3])
+            else:
+                boxPlot(melt(dFrame))
         elif sys.argv[2] == 'qq':
-            qq(dFrame)
+            if len(sys.argv) > 3:
+                qq(dFrame, sys.argv[3])
+            else:
+                qq(dFrame)
+        elif sys.argv[2] == 'histo':
+            if len(sys.argv) > 3:
+                histogram(dFrame, sys.argv[3])
+            else:
+                histogram(dFrame)
         elif sys.argv[2] == 'shapiro':
             shapiro(dFrame)
         elif sys.argv[2] == 'anova':
@@ -109,7 +131,10 @@ if __name__ == '__main__':
         elif sys.argv[2] == 't_hsd':
             t_hsd(dFrame)
         elif sys.argv[2] == 'resid':
-            resid(dFrame)             
+            if len(sys.argv) > 3:
+                resid(dFrame, sys.argv[3])
+            else:
+                resid(dFrame)             
         else:
             #if no valid argument is given
             help("Invalid argument given")
